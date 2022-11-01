dfc5496ed8f533e714fa66d9b24f2e984ec381f7
eronnen <elyronnen@gmail.com>
2022-11-02 01:17:20 +0200

    adding lichess to options page

--- pages/options.js
+++ pages/options.js
@@ -1,9 +1,32 @@
-let chesscomOldUsername = ''; 
+const WEBSITES = ["chesscom", "lichess"];
+const WEBSITES_NAMES = {"chesscom": "Chess.com", "lichess": "Lichess.org"};
+let oldUsernames = {'chesscom': '', 'lichess': '' };
+let usernameValidators = {
+    'chesscom': async function(username) {
+        return fetch(`https://api.chess.com/pub/player/${username}`, {method: 'GET', headers: {'Accept': 'application/json'}})
+            .then((response) => response.json())
+            .then((responseJson) => {
+                if (!responseJson['username']) {
+                    return false;
+                }
+                return true;
+            });
+    },
+    'lichess': async function(username) {
+        return fetch(`https://lichess.org/api/user/${username}`, {method: 'GET', headers: {'Accept': 'application/json'}})
+            .then((response) => response.json())
+            .then((responseJson) => {
+                if (!responseJson['username']) {
+                    return false;
+                }
+                return true;
+            });
+    }
+}
 
-function restore_day_limits(value) {
-    console.debug('restore day limit: ' + value)
+function restore_day_limits(website, limitType) {
     chrome.storage.sync.get({
-        chesscom_gamesPerDay: {
+        [website + '_gamesPerDay']: {
             0: 10,
             1: 10,
             2: 10,
@@ -13,123 +36,129 @@ function restore_day_limits(value) {
             6: 10,
         }
     }).then((items) => {
-        document.getElementById('chesscom.gamesPerDay').value = items.chesscom_gamesPerDay[0];
-        document.getElementById('chesscom.gamesPerDayMonday').value = items.chesscom_gamesPerDay[1];
-        document.getElementById('chesscom.gamesPerDayTuesday').value = items.chesscom_gamesPerDay[2];
-        document.getElementById('chesscom.gamesPerDayWednesday').value = items.chesscom_gamesPerDay[3];
-        document.getElementById('chesscom.gamesPerDayThursday').value = items.chesscom_gamesPerDay[4];
-        document.getElementById('chesscom.gamesPerDayFriday').value = items.chesscom_gamesPerDay[5];
-        document.getElementById('chesscom.gamesPerDaySaturday').value = items.chesscom_gamesPerDay[6];
-        document.getElementById('chesscom.gamesPerDaySunday').value = items.chesscom_gamesPerDay[0];
+        document.getElementById(website + '.gamesPerDay').value = items[website + '_gamesPerDay'][0];
+        document.getElementById(website + '.gamesPerDayMonday').value = items[website + '_gamesPerDay'][1];
+        document.getElementById(website + '.gamesPerDayTuesday').value = items[website + '_gamesPerDay'][2];
+        document.getElementById(website + '.gamesPerDayWednesday').value = items[website + '_gamesPerDay'][3];
+        document.getElementById(website + '.gamesPerDayThursday').value = items[website + '_gamesPerDay'][4];
+        document.getElementById(website + '.gamesPerDayFriday').value = items[website + '_gamesPerDay'][5];
+        document.getElementById(website + '.gamesPerDaySaturday').value = items[website + '_gamesPerDay'][6];
+        document.getElementById(website + '.gamesPerDaySunday').value = items[website + '_gamesPerDay'][0];
 
-        if (value == 'dayOfWeek') {
-            document.getElementById('li.chesscom.gamesPerDay').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDayMonday').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDayTuesday').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDayWednesday').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDayThursday').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDayFriday').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDaySaturday').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDaySunday').hidden = false;
+        if (limitType == 'dayOfWeek') {
+            document.getElementById(`li.${website}.gamesPerDay`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDayMonday`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDayTuesday`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDayWednesday`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDayThursday`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDayFriday`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDaySaturday`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDaySunday`).hidden = false;
         }
-        else {
-            document.getElementById('li.chesscom.gamesPerDay').hidden = false;
-            document.getElementById('li.chesscom.gamesPerDayMonday').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDayTuesday').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDayWednesday').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDayThursday').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDayFriday').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDaySaturday').hidden = true;
-            document.getElementById('li.chesscom.gamesPerDaySunday').hidden = true;
+        else { // 'day'
+            document.getElementById(`li.${website}.gamesPerDay`).hidden = false;
+            document.getElementById(`li.${website}.gamesPerDayMonday`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDayTuesday`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDayWednesday`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDayThursday`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDayFriday`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDaySaturday`).hidden = true;
+            document.getElementById(`li.${website}.gamesPerDaySunday`).hidden = true;
         }
     });
 }
 
 function restore_options() {
-    chrome.storage.sync.get({
+    optionsGetter = {
         dayStartTimeHours: 3,
-        dayStartTimeMinutes: 30,
-        chesscom_username: '',
-        chesscom_limitByWeekday: false
-    }).then((items) => {
-        chesscomOldUsername = items.chesscom_username;
+        dayStartTimeMinutes: 30
+    };
+
+    for (const website of WEBSITES) {
+        optionsGetter[website + "_username"] = '';
+        optionsGetter[website + "_limitByWeekday"] = false;
+    }
+
+    chrome.storage.sync.get(optionsGetter).then((items) => {
+        oldUsernames['chesscom'] = items.chesscom_username;
+        oldUsernames['lichess'] = items.lichess_username;
         document.getElementById('dayStartTime').value = `${items.dayStartTimeHours.toString().padStart(2,0)}:${items.dayStartTimeMinutes.toString().padStart(2,0)}`;
-        document.getElementById('chesscom.username').value = items.chesscom_username;
-        if (items.chesscom_limitByWeekday) {
-            document.getElementById('chesscom.limitByWeekDay').value = 'dayOfWeek';
+
+        for (const website of WEBSITES) {
+            document.getElementById(website + '.username').value = items[website + '_username'];
+            if (items[website + '_limitByWeekday']) {
+                document.getElementById(website + '.limitByWeekDay').value = 'dayOfWeek';
+            }
+            else {
+                document.getElementById(website + '.limitByWeekDay').value = 'day';
+            }
+            restore_day_limits(website, document.getElementById(website + '.limitByWeekDay').value);
         }
-        else {
-            document.getElementById('chesscom.limitByWeekDay').value = 'day';
-        }
-        restore_day_limits(document.getElementById('chesscom.limitByWeekDay').value);
     });
 }
 
-function save_options() {
+async function save_options() {
     const dayStartTime = document.getElementById('dayStartTime').value;
-    const dayStartTimeHours = dayStartTime.match(/(\d{2}):(\d{2})/)[1]; 
-    const dayStartTimeMinutes = dayStartTime.match(/(\d{2}):(\d{2})/)[2]; 
-    let chesscomUsername = document.getElementById('chesscom.username').value;
-    const limitByWeekDay = document.getElementById('chesscom.limitByWeekDay').value == 'dayOfWeek';
-    const chesscomGamesPerDay = {
-        1: document.getElementById('chesscom.gamesPerDayMonday').value,
-        2: document.getElementById('chesscom.gamesPerDayTuesday').value,
-        3: document.getElementById('chesscom.gamesPerDayWednesday').value,
-        4: document.getElementById('chesscom.gamesPerDayThursday').value,
-        5: document.getElementById('chesscom.gamesPerDayFriday').value,
-        6: document.getElementById('chesscom.gamesPerDaySaturday').value,
-        0: document.getElementById('chesscom.gamesPerDaySunday').value,
-    };
-    if (!limitByWeekDay) {
-        chesscomGamesPerDay[0] = document.getElementById('chesscom.gamesPerDay').value;
-        chesscomGamesPerDay[1] = document.getElementById('chesscom.gamesPerDay').value;
-        chesscomGamesPerDay[2] = document.getElementById('chesscom.gamesPerDay').value;
-        chesscomGamesPerDay[3] = document.getElementById('chesscom.gamesPerDay').value;
-        chesscomGamesPerDay[4] = document.getElementById('chesscom.gamesPerDay').value;
-        chesscomGamesPerDay[5] = document.getElementById('chesscom.gamesPerDay').value;
-        chesscomGamesPerDay[6] = document.getElementById('chesscom.gamesPerDay').value;
-    }
+    const dayStartTimeHours = dayStartTime.match(/(\d{2}):(\d{2})/)[1];
+    const dayStartTimeMinutes = dayStartTime.match(/(\d{2}):(\d{2})/)[2];
+
     const saveButton = document.getElementById('save');
     saveButton.disabled = true;
 
-    let usernameCheckPromise;
-    if (chesscomUsername != chesscomOldUsername) {
-        usernameCheckPromise = 
-            fetch(`https://api.chess.com/pub/player/${chesscomUsername}`, {method: 'GET', headers: {'Accept': 'application/json'}})
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    if (!responseJson['username']) {
-                        alert(`username "${chesscomUsername}" doesn't exist in chess.com!`);
-                        chesscomUsername = '';
-                    }
-                });
-    }
-    else {
-        usernameCheckPromise = Promise.resolve();
-    }
+    for (const website of WEBSITES) {
+        let username = document.getElementById(website + '.username').value;
+        const limitByWeekDay = document.getElementById(website + '.limitByWeekDay').value == 'dayOfWeek';
+        const gamesPerDay = {
+            1: document.getElementById(website + '.gamesPerDayMonday').value,
+            2: document.getElementById(website + '.gamesPerDayTuesday').value,
+            3: document.getElementById(website + '.gamesPerDayWednesday').value,
+            4: document.getElementById(website + '.gamesPerDayThursday').value,
+            5: document.getElementById(website + '.gamesPerDayFriday').value,
+            6: document.getElementById(website + '.gamesPerDaySaturday').value,
+            0: document.getElementById(website + '.gamesPerDaySunday').value,
+        };
+        if (!limitByWeekDay) {
+            gamesPerDay[0] = document.getElementById(website + '.gamesPerDay').value;
+            gamesPerDay[1] = document.getElementById(website + '.gamesPerDay').value;
+            gamesPerDay[2] = document.getElementById(website + '.gamesPerDay').value;
+            gamesPerDay[3] = document.getElementById(website + '.gamesPerDay').value;
+            gamesPerDay[4] = document.getElementById(website + '.gamesPerDay').value;
+            gamesPerDay[5] = document.getElementById(website + '.gamesPerDay').value;
+            gamesPerDay[6] = document.getElementById(website + '.gamesPerDay').value;
+        }
+        
+        if (oldUsernames[website] != username) {
+            let isUsernameValid = await usernameValidators[website](username);
+            if (!isUsernameValid) {
+                alert(`username "${username}" doesn't exist in ${WEBSITES_NAMES[website]}`);
+                username = '';
+            }
+        }
 
-    usernameCheckPromise.then(() => {
-        chesscomOldUsername = chesscomUsername;
-        document.getElementById('chesscom.username').value = chesscomUsername;
-    }).then(() => {
-        return chrome.storage.sync.set({
+        oldUsernames[website] = username;
+        document.getElementById(website + '.username').value = username;
+
+        await chrome.storage.sync.set({
             dayStartTimeHours: dayStartTimeHours,
             dayStartTimeMinutes: dayStartTimeMinutes,
-            chesscom_username: chesscomUsername,
-            chesscom_gamesPerDay: chesscomGamesPerDay,
+            [website + '_username']: username,
+            [website + '_limitByWeekday']: limitByWeekDay,
+            [website + '_gamesPerDay']: gamesPerDay,
         });
-    }).then(() => {
-        const status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(() => {
-            status.textContent = '';
-            saveButton.disabled = false;
-        }, 1000);
-    });
+    }
+
+    const status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(() => {
+        status.textContent = '';
+        saveButton.disabled = false;
+    }, 1000);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('chesscom.limitByWeekDay').addEventListener('change', (event) => { restore_day_limits(event.target.value); });
+    for (const website of WEBSITES) {
+        document.getElementById(website + '.limitByWeekDay').addEventListener('change', (event) => { restore_day_limits(website, event.target.value); });
+    }
     restore_options();
 });
 document.getElementById('save').addEventListener('click', save_options);
